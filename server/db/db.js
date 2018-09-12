8a5db1b5db45fa6313ccd2e96dc6b90c9869b72f
Travis Margetts <travismargetts@gmail.com>
2018-09-11 19:47:22 -0500

    updating postgres path in db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5433/pupper', {
+  process.env.DATABASE_URL || 'postgres://paovlcovfnsiyj:9d9ba23183e6dd220270dac177d97ceadaa05b9b9909d8896a7ab5597976b8b9@ec2-23-23-216-40.compute-1.amazonaws.com:5432/db9u0do3rd5k4d', {
     logging: false,
     operatorsAliases: false,
   }
